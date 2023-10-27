ee7066e1d04e74284d0b3ee4c8d66a53e81d711c
DO_S <rlaxogus7094@naver.com>
2023-10-27 14:31:19 +0900

    feat : 키보드 컨트롤 기능 추가
    불필요 파일 제거 및 파일 통합도 함께 진행함

--- main.py
+++ main.py
@@ -1,21 +1,86 @@
 import cv2
-from cvzone.HandTrackingModule import HandDetector
+import mediapipe as mp
+import numpy as np
+import keyboard
 
-# WebCam 을 사용할 경우
+max_num_hands = 2
+gesture = {
+    0:'fist', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+    6:'six', 7:'rock', 8:'spiderman', 9:'yeah', 10:'ok',
+} # 사용 안함
+rps_gesture = {0:'rock', 5:'paper', 9:'scissors'}
+
+flag = False
+
+# MediaPipe Hand landmark 모델
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    max_num_hands=max_num_hands,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+# 제스쳐 인식 모델
+file = np.genfromtxt('data/gesture_train.csv', delimiter=',')
+angle = file[:,:-1].astype(np.float32)
+label = file[:, -1].astype(np.float32)
+knn = cv2.ml.KNearest_create()
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)
+
 cap = cv2.VideoCapture(0)
 
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        continue
 
-# 손을 감지
-detector = HandDetector(maxHands=2, detectionCon=0.8)
+    img = cv2.flip(img, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-while True:
-    # 웹켐에서 프레임 가져오기
-    success, img = cap.read()
+    result = hands.process(img)
 
-    # Hands
-    hands, img = detector.findHands(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
-    cv2.imshow("Image", img)
+    if result.multi_hand_landmarks is not None:
+        for res in result.multi_hand_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
 
-    if cv2.waitKey(1) == ord("q"): # q 누를 시 웹켐 종료
+            # 관절끼리의 각도 계산
+            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:] # Parent joint
+            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:] # Child joint
+            v = v2 - v1 # [20,3]
+            # v로 정규화
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            # Get angle using arcos of dot product
+            angle = np.arccos(np.einsum('nt,nt->n',
+                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:],
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:])) # [15,]
+
+            angle = np.degrees(angle) # Convert radian to degree
+
+            # 제스처 인터페이스
+            data = np.array([angle], dtype=np.float32)
+            ret, results, neighbours, dist = knn.findNearest(data, 3)
+            idx = int(results[0][0])
+
+            # 결과값 인터페이스에 표기
+            if idx in rps_gesture.keys():
+                cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+
+                print(res.landmark)
+
+                if rps_gesture[idx] == 'rock':
+                    if not flag:
+                        keyboard.send("space")
+                        flag = True
+                else:
+                    flag = False
+
+            mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow('Game', img)
+    if cv2.waitKey(1) == ord('q'):
         break
